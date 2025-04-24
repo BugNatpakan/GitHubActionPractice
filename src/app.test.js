5a1345db4422b219c143b8c3695df61a03475711
BugNatpakan <somboonnatpacan@gmail.com>
2025-04-24 13:59:41 +0700

    update app.js and app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -7,6 +7,6 @@ test('getDay returns the long-format day of the week', () => {
 });
 
 test('getDay returns the month of the year', () => {
-    const day = monthOfTheYear( new Date('3/11/2020') );
-    expect( day ).toBe('November');
+    const month = monthOfTheYear( new Date('3/11/2020') );
+    expect( month ).toBe('November');
 });
